113e832e27195000d45bbe53c120caaa6e65f050
vikash <vikashp@tifrh>
2024-08-11 18:58:28 +0200

    sticking added

---
+++ memc/src/sticking.hpp
@@ -0,0 +1,20 @@
+#ifndef STICKING_HPP
+#define STICKING_HPP
+#include <string>
+#include <vector>
+#include "mesh.hpp"
+#include "vector.hpp"
+
+// #include "global.h"
+
+class STICK {
+public : 
+  double stick_energy_total(Vec3d *pos, int);
+ double stick_energy_ipart(Vec3d pos);
+ int initSTICK(int , std::string );
+private:
+    double epsilon;  // strength of stick potential
+    double sigma, pos_bot_wall;
+};
+
+#endif

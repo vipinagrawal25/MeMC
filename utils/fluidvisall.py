d5076d3cf0ee96c73f8c06b918226cd1767bfcf1
Vipin Agrawal <vipin.agrawal@northwestern.edu>
2024-10-25 15:49:05 -0500

    new fluidvisall

--- utils/fluidvisall.py
+++ utils/fluidvisall.py
@@ -10,15 +10,16 @@ def lastfile(dirname,prefix='',zfill=5,suffix=".h5"):
     low=int(lastF.split("/")[-1].replace(prefix,"").replace(suffix,""))
     return low
 ################################################################################
-dd=sys.argv[1]
-nfiles=lastfile(dd,"snap_")
-if len(sys.argv)==2:
-    jump=1
-else:
-    jump=int(sys.argv[2])
-for i in range(0,nfiles,jump):
-	cmd="python utils/fluidvis.py "
-	cmd+=dd.replace("/mc_log","/")+"snap_"+str(i).zfill(5)+".h5"
-	print(cmd)
-	os.system(cmd)
+for dd in sys.argv[1:]:
+    # dd=sys.argv[1]
+    nfiles=lastfile(dd,"snap_")
+    # if len(sys.argv)==2:
+    #     jump=1
+    # else:
+    #     jump=int(sys.argv[2])
+    for i in range(0,nfiles):
+        cmd="python utils/fluidvis.py "
+        cmd+=dd.replace("/mc_log","/")+"snap_"+str(i).zfill(5)+".h5"
+        print(cmd)
+        os.system(cmd)
 ################################################################################

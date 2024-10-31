40bad56c5cc9261c38bf9f62e3cfa597143e86ca
Vipin Agrawal <vipin.agrawal@northwestern.edu>
2024-10-31 18:35:06 -0500

    added Gaussian energy

--- src/bending.cpp
+++ src/bending.cpp
@@ -1,7 +1,7 @@
 #include "bending.hpp"
 #include "multicomp.hpp"
 #include <fstream>
-
+#import <cmath>
 #define sign(x) ((x > 0) ? 1 : ((x < 0) ? -1 : 0))
 
 extern "C" void BendRead(double *, double *, double *, double *, char *);
@@ -31,6 +31,7 @@ BE::BE(const MESH_p& mesh, std::string fname){
       << " spC1 " << spC1 << endl
       << " spC2 " << spC2 << endl;
     out_.close();
+
 }
 /*-------------------------------------------------*/
 void BE::init_coefbend(int *lipA, int N){
@@ -40,31 +41,35 @@ void BE::init_coefbend(int *lipA, int N){
         else coef_bend.push_back(bend1);
     }
 }
+/*--------------------------------------------------------------------------*/
+inline double acot(double x) {
+    return atan(1.0 / x);
+}
 /*-------------------------------------------------*/
-// pair<>
-/*-------------------------------------------------*/
+// There is a problem if we have the vertices on the polls -- zaxis.
 double BE::bending_energy_ipart(Vec3d *pos, int *node_nbr, int num_nbr, int idx,
             int bdry_type, double lenth, int edge){
     /// @brief Estimate the Bending energy contribution when ith particle 
     /// position changes
-    ///  @param Pos array containing co-ordinates of all the particles
-    ///  @param idx index of ith particle;
-    ///  @param node_nbr nearest neigbours of idx; 
-    ///  @param num_nbr number of nearest neigbours of idx; 
-    ///  @param para  Membrane related parameters;
+    /// @param Pos array containing co-ordinates of all the particles
+    /// @param idx index of ith particle;
+    /// @param node_nbr nearest neigbours of idx; 
+    /// @param num_nbr number of nearest neigbours of idx; 
+    /// @param para  Membrane related parameters;
     /// @todo try openMP Pragmas;
     /// @return Bending Energy contribution when ith particle is displaced.
-    double bend_ener;
+    double bend_ener, Gauss_ener;
     Vec3d cot_times_rij;
-    Vec3d lap_bel,lap_bel_t0,nhat;
-    double cot_aij[num_nbr],cot_bij[num_nbr],area_ijk[num_nbr];
-    double ljksq[num_nbr], lijsq[num_nbr];
-    Vec3d xij[num_nbr],xjk;
+    Vec3d lap_bel,lap_bel_t0, nhat;
+    Vec3d xjk;
     double cot_jdx_k,cot_kdx,cot_kmdx,area_ijkm;
     Vec3d xik,xikm,xjkm,nhat_local,xijp1;
     int jdx,kdx,kmdx,jdxp1;
-    double cot_sum,liksq,likmsq,ljkmsq;
+    double liksq,likmsq,ljkmsq;
     double sigma_i = 0e0;
+    double cot_aij[num_nbr],cot_bij[num_nbr],area_ijk[num_nbr];
+    double ljksq[num_nbr], lijsq[num_nbr];
+    Vec3d xij[num_nbr];
     // store all the lengths
     if (bdry_type == 1 || idx>edge){
         for (int j = 0; j < num_nbr; ++j){
@@ -102,21 +107,33 @@ double BE::bending_energy_ipart(Vec3d *pos, int *node_nbr, int num_nbr, int idx,
         likmsq=lijsq[(j-1+num_nbr)%num_nbr];
         area_ijkm=area_ijk[(j-1+num_nbr)%num_nbr];
         xijp1=xij[(j+1)%num_nbr];
-        cot_sum=cot_aij[j] + cot_bij[j];
-        cot_times_rij = cot_times_rij + xij[j]*cot_sum;
+        cot_times_rij = cot_times_rij + xij[j]*(cot_aij[j] + cot_bij[j]);
         sigma_i=sigma_i+voronoi_area(cot_jdx_k,cot_bij[j],liksq,lijsq[j],area_ijk[j]);
         nhat_local=cross_product(xijp1,xij[j]);
         nhat = nhat + nhat_local*(1e0/norm(nhat_local));
     }
+    // cout << idx << " "<< norm(nhat) << endl;
     nhat = nhat/norm(nhat);
+    // if (idx==18||idx==23){
+    //     cout << sigma_i << endl;
+    // }
     lap_bel = cot_times_rij/sigma_i;
-    lap_bel = lap_bel*0.5;
+    lap_bel = lap_bel*0.5; 
     lap_bel_t0 = nhat*spcurv;
-    bend_ener = 0.5*coef_bend[idx]*sigma_i*normsq(lap_bel-lap_bel_t0);
+    bend_ener = 0.5*sigma_i*normsq(lap_bel-lap_bel_t0);
+    if (iGauss){
+        Gauss_ener = M_PI*(2-num_nbr);
+        for (int j = 0; j < num_nbr; ++j){
+            Gauss_ener+=acot(cot_aij[j])+acot(cot_bij[j]);
+        }
+        // print(pos[idx]);
+        // cout  << " " << bend_ener/sigma_i << " " << Gauss_ener << endl;
+    }
     return bend_ener;
 }
 /*--------------------------------------------------------------------------*/
-double BE::bending_energy_ipart_neighbour(Vec3d *pos, MESH_p mesh, int idx){
+double BE::bending_energy_ipart_neighbour(Vec3d *pos, MESH_p mesh, 
+    int idx){
     /// @brief Estimate the Bending energy contribution from the neighbours 
     /// when ith particle position changes
     /// @param Pos array containing co-ordinates of all the particles
@@ -126,8 +143,7 @@ double BE::bending_energy_ipart_neighbour(Vec3d *pos, MESH_p mesh, int idx){
     int j;
     int num_nbr_j;
     int nbr, cm_idx_nbr;
-    double be;
-    be = 0e0;
+    double be=0e0;
     for(j = idx*mesh.nghst; j < idx*mesh.nghst + mesh.numnbr[idx]; j++){
         nbr = mesh.node_nbr_list[j];
         num_nbr_j = mesh.numnbr[nbr];
@@ -148,18 +164,19 @@ double BE::bending_energy_total(Vec3d *pos, MESH_p mesh){
     /// @return Total Bending energy
     int idx, st_idx;
     int num_nbr, cm_idx;
-    double be;
-    be = 0e0;
+    double be=0e0;
     //
     st_idx = get_nstart(mesh.N, mesh.bdry_type);
-    
+    //
     for(idx = st_idx; idx < mesh.N; idx++){
-        /* idx = 2; */
+        /* idx = 2; */        
         cm_idx = idx*mesh.nghst;
         num_nbr = mesh.numnbr[idx];
-        be += bending_energy_ipart(pos, (int *) (mesh.node_nbr_list + cm_idx),
+        be+= bending_energy_ipart(pos, (int *) (mesh.node_nbr_list + cm_idx),
                 num_nbr, idx, mesh.bdry_type, mesh.boxlen, mesh.edge);
     }
+    cout << be << endl;
+    exit(1);
     return be;
 }
 /*------------------------------------------------------------------------------*/
@@ -168,8 +185,4 @@ void BE::exchange(int idx1, int idx2){
    coef_bend[idx1] = coef_bend[idx2];
    coef_bend[idx2] = temp;
 }
-// /*-------------------------------------------------------------------------------------*/
-// double BE::Gaussian_energy_ipart(Vec3d *pos, int *node_nbr, int num_nbr, int idx,
-//     int bdry_type, double lenth, int edge){
-
-// }
+/*------------------------------------------------------------------------------*/
